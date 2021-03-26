539a999884001b208dc8b9cea801c023f42b0e9f
kardash-bp <kardasbp@gmail.com>
2021-03-26 20:21:51 +0100

    css

--- src/components/parts/Sidebar.js
+++ src/components/parts/Sidebar.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faSearch } from '@fortawesome/free-solid-svg-icons'
 export default class Sidebar extends Component {
   handleChange = (e) => {
     this.props.findContact(e.target.value)
   }
   render() {
     return (
-      <form className=' my-2 my-lg-0'>
+      <form className='my-2 my-lg-0 pt-4'>
         <input
           className='form-control '
           type='search'
